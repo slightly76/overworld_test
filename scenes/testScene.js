81d607cd7b878829354e3a52bb1a631b4ebd9441
Ken'Terria Reaves <kenterria.z.reaves@gmail.com>
2025-04-16 10:51:34 +0100

    feat: scenes for each map and event triggers for map transitions, etc

--- scenes/testScene.js
+++ scenes/testScene.js
@@ -1,68 +1,90 @@
-import Phaser from 'phaser';
-import Player from '../src/player.js';
+import Phaser from "phaser";
+import Player from "../src/player.js";
 
 export default class overworldScene extends Phaser.Scene {
-	constructor() {
-		super('overworldScene');
-	}
+  constructor() {
+    super("overworldScene");
+  }
 
-	preload() {
-		this.load.tilemapTiledJSON('map', '../assets/overworld.JSON');
-		this.load.image('mapImage', '../assets/1_Terrains_32x32.png');
-		this.load.spritesheet('playerSheet', '../assets/farmer.png', {
-			frameWidth: 64,
-			frameHeight: 64,
-		});
-		this.load.audio('unacceptable', '../assets/unacceptable.mp3');
-	}
+  preload() {
+    this.load.tilemapTiledJSON("map", "../assets/overworld.JSON");
+    this.load.image("mapImage", "../assets/1_Terrains_32x32.png");
+    this.load.spritesheet("playerSheet", "../assets/dummy.png", {
+      frameWidth: 64,
+      frameHeight: 64,
+    });
+  }
 
-	create() {
-		//scene fades in
-		this.cameras.main.fadeIn(1000, 0, 0, 0);
+  create() {
+    this.spaceKey = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.SPACE
+    );
 
-		// create map
-		const map = this.make.tilemap({ key: 'map' });
-		const tilesets = map.addTilesetImage('1_Terrains_32x32', 'mapImage');
-		const mapLayer = map.createLayer('grass', tilesets, 0, 0);
-		mapLayer.setCollisionByProperty({ collide: true });
-		this.locationLayer = map.createLayer('locations', tilesets, 0, 0);
-		this.locationLayer.setCollisionByProperty({ collide: true });
+    this.cameras.main.fadeIn(1000, 0, 0, 0);
 
-		//create hidden trigger sprite for doors etc
-		this.doorTrigger = this.physics.add.sprite(272, 240, null);
-		this.doorTrigger.setSize(42, 42);
-		this.doorTrigger.setVisible(false);
-		this.doorTriggered = false;
+    const map = this.make.tilemap({ key: "map" });
+    const tilesets = map.addTilesetImage("1_Terrains_32x32", "mapImage");
+    const mapLayer = map.createLayer("grass", tilesets, 0, 0);
+    mapLayer.setCollisionByProperty({ collide: true });
+    this.locationLayer = map.createLayer("locations", tilesets, 0, 0);
+    this.locationLayer.setCollisionByProperty({ collide: true });
 
-		//create player and add collision rules
-		this.player = new Player(this, 250, 300, 'playerSheet');
-		this.physics.add.collider(this.player, mapLayer);
-	}
+    //create hidden trigger sprite for doors etc
+    this.farmSceneTrigger = this.physics.add.sprite(272, 240, null);
+    this.farmSceneTrigger.setSize(42, 42);
+    this.farmSceneTrigger.setVisible(false);
+    this.farmSceneTriggered = false;
 
-	update() {
-		this.player.update();
+    this.sceneTriggers = [
+      { name: "farmScene", x: 272, y: 240 },
+      { name: "officeScene", x: 300, y: 320 },
+      { name: "battleScene", x: 120, y: 420 },
+      //ADVANCED TODO: TECH DUNGEON
+      // { name: "techDungeon", x: 120, y: 420 },
+    ];
 
-		const playerBounds = new Phaser.Geom.Rectangle(
-			this.player.x - this.player.width / 2 + 24,
-			this.player.y - this.player.height / 2 + 38,
-			15,
-			8
-		);
-		//create doortrigger overlap rules
-		const isOverlapping = Phaser.Geom.Intersects.RectangleToRectangle(
-			playerBounds,
-			this.doorTrigger.getBounds()
-		);
-		if (isOverlapping && !this.doorTriggered) {
-			this.doorTriggered = true;
-			console.log('UNACCEPTABLE OVERLAP!!!');
+    this.triggers = this.sceneTriggers.map((trigger) => {
+      const zone = this.physics.add.sprite(trigger.x, trigger.y, null);
+      zone.setSize(42, 42);
+      zone.setVisible(false);
+      zone.sceneName = trigger.name;
+      return zone;
+    });
 
-			this.sound.play('unacceptable');
-			this.time.delayedCall(1000, () => {
-				this.cameras.main.shake(1000, 0.01);
-			});
-		} else if (!isOverlapping && this.doorTriggered) {
-			this.doorTriggered = false;
-		}
-	}
+    //create player and add collision rules
+    this.player = new Player(this, 250, 300, "playerSheet");
+    this.physics.add.collider(this.player, mapLayer);
+  }
+
+  moveScene(sceneName) {
+    this.input.keyboard.enabled = false;
+    this.cameras.main.fadeOut(1000, 0, 0, 0);
+    this.time.delayedCall(1000, () => {
+      this.scene.start(sceneName);
+      this.input.keyboard.enabled = true;
+    });
+  }
+
+  update() {
+    this.player.update();
+
+    const playerBounds = new Phaser.Geom.Rectangle(
+      this.player.x - this.player.width / 2 + 24,
+      this.player.y - this.player.height / 2 + 38,
+      15,
+      8
+    );
+
+    this.triggers.forEach((trigger) => {
+      if (
+        Phaser.Geom.Intersects.RectangleToRectangle(
+          playerBounds,
+          trigger.getBounds()
+        ) &&
+        Phaser.Input.Keyboard.JustDown(this.spaceKey)
+      ) {
+        this.moveScene(trigger.sceneName);
+      }
+    });
+  }
 }
